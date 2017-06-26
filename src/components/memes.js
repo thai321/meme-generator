b1ce33fbb8c96c71e585ae5b3f76782496023999
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-26 04:52:00 -0700

    add some custome styles for meme app

--- src/components/memes.js
+++ src/components/memes.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
 class MemeItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hover: false
+    }
+  }
 
   render() {
     const { name, url } =this.props.meme;
     return (
-      <div className="meme-item">
+      <div className="meme-item"
+        onMouseOver={() => this.setState({hover: true})}
+        onMouseLeave={() => this.setState({hover: false})}
+      >
         <img
-          className="meme-img"
+          className={this.state.hover ? "meme-img darken-img" : "meme-img"}
           src={url}
           alt={name}
           />
-        <p className="meme-txt">{name}</p>
+
+        <p className={this.state.hover ? "meme-txt" : "no-txt"}>{name}</p>
+
       </div>
     )
   }
