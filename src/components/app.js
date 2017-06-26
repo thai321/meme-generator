ded450f4f056aeccb92bd9dcf9e9b805d08bc706
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-26 02:57:25 -0700

    first commit of install and run the app

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Mem Generator!</h2>
+      </div>
+    )
+  }
+}
+
+
+export default App;
